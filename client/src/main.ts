5811886817efd1334aae9d734b113841557fc2a6
刘孝林 <13106917538@163.com>
2023-04-25 10:38:52 +0800

    修改文件目录层级，搭建基础服务端架构

---
+++ client/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './assets/css/index.less'
+import App from './App'
+
+const app = createApp(App)
+app.mount('#app')
---
+++ client/vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import variables from './src/assets/css/variables'
+import vueJSX from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJSX(), // 支持jsx语法
+  ],
+  resolve: {
+    // 导入别名
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      "/images": "src/assets/images",
+    }
+  },
+  server: {
+    open: true,
+    host: '0.0.0.0',
+    cors: true,
+    port: 3000
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          // reference:  Avoid repeated references
+          // hack: `true; @import (reference) "${resolve('src/design/config.less')}";`,
+          ...variables,
+        },
+        javascriptEnabled: true,
+      },
+    },
+  }
+})
